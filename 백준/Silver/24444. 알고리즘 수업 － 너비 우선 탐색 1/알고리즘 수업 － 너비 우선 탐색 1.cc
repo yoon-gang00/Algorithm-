3e6b4c67afcee23652fc90568160b8825ae3054f
yoon-gang <76502911+yoon-gang00@users.noreply.github.com>
2023-02-18 14:51:00 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 88 ms, Memory: 9648 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#define MAX 100001
+using namespace std;
+
+vector<int> vec[MAX];
+int v_order[MAX];
+bool visit[MAX];
+int order = 0;
+int n, m, r;
+void bfs(int n) {
+    queue<int> q;
+    q.push(n);
+    visit[n] = 1;
+    order++;
+    v_order[n] = order;
+    while (!q.empty()) {
+        int x = q.front();
+        q.pop();
+        for (int i = 0; i < vec[x].size(); i++) {
+            int tmp = vec[x][i];
+            if (!visit[tmp]) {
+                order++;
+                v_order[tmp] = order;
+                q.push(tmp);
+                visit[tmp] = 1;
+            }
+        }
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m >> r;
+    for (int i = 1; i <= m; i++) {
+        int v, u;
+        cin >> v >> u;
+        vec[v].push_back(u);
+        vec[u].push_back(v);
+    }
+    for (int i = 1; i <= n; i++)
+        sort(vec[i].begin(), vec[i].end());
+    bfs(r);
+    for (int i = 1; i <= n; i++) {
+        cout << v_order[i] << "\n";
+    }
+    return 0;
+}
