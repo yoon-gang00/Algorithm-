99b5a48ef4b789813bec3aa8992729626d391df7
yoon-gang <76502911+yoon-gang00@users.noreply.github.com>
2023-02-18 14:30:09 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 112 ms, Memory: 11028 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MAX 100001
+using namespace std;
+
+vector<int> vec[MAX];
+int v_order[MAX];
+bool visit[MAX];
+int order = 0;
+int n, m, r;
+void dfs(int n) {
+    if (!visit[n]) {
+        visit[n] = true;
+        order++;
+        v_order[n] = order;
+    }
+    else {
+        return;
+    }
+    for (int i = 0; i < vec[n].size(); i++) {
+        int next = vec[n][i];
+        dfs(next);
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m >> r;
+    for (int i = 1; i <= m; i++) {
+        int v, u;
+        cin >> v >> u;
+        vec[v].push_back(u);
+        vec[u].push_back(v);
+    }
+    for (int i = 1; i <= n; i++)
+        sort(vec[i].begin(), vec[i].end(),greater<int>());
+    dfs(r);
+    for (int i = 1; i <= n; i++) {
+        cout << v_order[i] << "\n";
+    }
+    return 0;
+}
