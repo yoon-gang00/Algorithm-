7025548fe19a6d613247bc41bf156f0845a0e042
yoon-gang <76502911+yoon-gang00@users.noreply.github.com>
2023-03-01 19:08:45 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = n - i; j > 0; j--) {
+			cout << " ";
+		}
+		for (int j = 2 * i - 1; j > 0; j--) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			cout << " ";
+		}
+		for (int j = 2 * (n - i) - 1; j > 0; j--) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+}
