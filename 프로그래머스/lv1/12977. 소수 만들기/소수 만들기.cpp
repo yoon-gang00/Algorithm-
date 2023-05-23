8d68618bf10ac14d3b1df543424d321115cfca5b
yoon-gang <yk1008@konkuk.ac.kr>
2023-05-23 12:43:07 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+bool prime(int num){
+    if(num==0||num==1)
+        return false;
+    for(int i=2;i<num/2;i++){
+        if(num%i==0) return false;
+    }
+    return true;
+}
+int solution(vector<int> nums) {
+    int answer = 0;
+    for(int i=0;i<nums.size();i++){
+        for(int j=i+1;j<nums.size();j++){
+            for(int k=j+1;k<nums.size();k++){
+                int n = nums[i]+nums[j]+nums[k];
+                if(prime(n)==true){
+                    answer++;
+                }
+            }
+        }
+    }
+    
+    return answer;
+}
