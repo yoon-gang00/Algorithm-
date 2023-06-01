214140e6fb677ecc83723282aabbdb0810b64ac7
yoon-gang <yk1008@konkuk.ac.kr>
2023-06-01 13:50:04 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.09 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int number, int limit, int power) {
+    int answer = 0;
+    for(int i=1;i<=number;i++){
+        int cnt=0;
+        for(int j=1;(j*j)<=i;j++){
+            if(i%j==0){
+                int x = (int)(i/j);
+                if((x*x)==i){
+                    cnt+=1;
+                }
+                else
+                    cnt +=2;
+            }
+            
+        }
+        if(cnt>limit){
+            answer += power;
+        }
+        else{
+            answer += cnt;
+        }
+    }
+    
+    return answer;
+}
