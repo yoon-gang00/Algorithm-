7c78d4caf895e090e2a8fa49d64987403b262f93
yoon-gang <yk1008@konkuk.ac.kr>
2023-07-01 00:00:33 +0900

    [Bronze III] Title: 세 막대, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int a[3];
+    cin >> a[0]>>a[1]>>a[2];
+    sort(a,a+3);
+    if(a[0]+a[1]>a[2])
+        cout << a[0]+a[1]+a[2];
+    else
+        cout << (a[0]+a[1])*2-1;
+    
+}
