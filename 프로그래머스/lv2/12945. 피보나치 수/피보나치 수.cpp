8dfad7701feaf0800dcb29caa01107e306df90a5
yoon-gang <yk1008@konkuk.ac.kr>
2023-08-03 21:35:52 +0900

    [level 2] Title: 피보나치 수, Time: 0.50 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int solution(int n) {
+    int answer = 0;
+    int fibo[100001];
+    fibo[0]=0;
+    fibo[1]=1;
+    for(int i=2;i<=n;i++){
+        fibo[i]=(fibo[i-1]+fibo[i-2])%1234567;
+    }
+   
+    answer = fibo[n];
+    return answer;
+}
