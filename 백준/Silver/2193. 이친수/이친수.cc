653be195e6ced74d57ae593da9a1166afe99dbee
yoon-gang <yk1008@konkuk.ac.kr>
2023-10-11 12:48:51 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    long long dp[91][2] {0, };
+    int n;
+    cin >> n;
+    dp[1][0] = 0;
+    dp[1][1] = 1;
+    for(int i=2;i<=n;i++){
+        dp[i][0] += dp[i-1][0];
+        dp[i][0] += dp[i-1][1];
+        dp[i][1] += dp[i-1][0];
+    }
+    cout << dp[n][0] + dp[n][1] << '\n';
+    return 0;
+}
