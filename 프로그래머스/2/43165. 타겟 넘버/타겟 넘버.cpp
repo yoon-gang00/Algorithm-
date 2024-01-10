99c50a49844a400a8a34c92d5dda6922db365672
yoon-gang <sgi07151@naver.com>
2024-01-10 21:28:51 +0900

    [level 2] Title: 타겟 넘버, Time: 0.98 ms, Memory: 3.71 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int answer=0;
+void dfs(vector<int> numbers, int target, int sum, int idx){
+    if(idx==numbers.size()){
+        if(sum == target){
+            answer++;
+        }
+        return;
+    }
+        dfs(numbers, target, sum+numbers[idx],idx+1);
+        dfs(numbers, target, sum-numbers[idx],idx+1);
+}
+int solution(vector<int> numbers, int target) {
+    dfs(numbers,target,0,0);
+    return answer;
+}
