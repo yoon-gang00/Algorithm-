252fce6e857f4b9ab4a875505ed85d0325059462
yoon-gang <sgi07151@naver.com>
2024-03-29 20:11:20 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.cc
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 int main(){
-    long long dp[91][2] {0, };
     int n;
+    long ans=0;
     cin >> n;
-    dp[1][0] = 0;
-    dp[1][1] = 1;
-    for(int i=2;i<=n;i++){
-        dp[i][0] += dp[i-1][0];
-        dp[i][0] += dp[i-1][1];
-        dp[i][1] += dp[i-1][0];
+    long arr[91][2];
+    arr[1][0]=0;
+    arr[1][1]=1;
+    for(int i=1;i<n;i++){
+        arr[i+1][0] = arr[i][0]+arr[i][1];
+        arr[i+1][1] = arr[i][0];
     }
-    cout << dp[n][0] + dp[n][1] << '\n';
-    return 0;
+   ans = arr[n][0] + arr[n][1];
+ cout << ans;
 }
