69a85bee33e6c90a813041be0627dec4c9ae95fe
yoon-gang <sgi07151@naver.com>
2024-04-22 23:46:07 +0900

    [Silver IV] Title: 상근이의 여행, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    for(int i=0;i<t;i++){
+        int n,m;
+        cin >> n >> m;
+        for(int j=0;j<m;j++){
+            int a,b;
+            cin >> a >> b;
+        }
+        cout << n-1<<"\n";
+    }
+}
