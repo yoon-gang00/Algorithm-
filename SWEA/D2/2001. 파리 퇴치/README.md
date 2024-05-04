011c2ea2c57434aa9f57b5216afa544ea404b468
yoon-gang <sgi07151@naver.com>
2024-05-04 23:44:43 +0900

    [D2] Title: 파리 퇴치, Time: 9 ms, Memory: 13,384 KB -BaekjoonHub

---
+++ SWEA/D2/2001. 파리 퇴치/README.md
@@ -0,0 +1,15 @@
+# [D2] 파리 퇴치 - 2001 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PzOCKAigDFAUq) 
+
+### 성능 요약
+
+메모리: 13,384 KB, 시간: 9 ms, 코드길이: 809 Bytes
+
+### 제출 일자
+
+2024-05-04 23:44
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	cin>>T;
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+
+	    int n,m;
+	    cin >> n >> m;
+	    vector<vector<int>> v(n, vector<int>(n));
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<n;j++){
+	            cin >> v[i][j];
+	        }
+	    }
+    int t=(n-m+1);
+    int ans=0;
+    int max_ans=0;
+    for(int i=0;i<t;i++){
+        for(int j=0;j<t;j++){
+            for(int x=0;x<m;x++){
+                for(int y=0;y<m;y++){
+                    ans+=v[i+x][j+y];
+                }
+            }
+            max_ans = max(ans,max_ans);
+            ans=0;
+        }
+        
+    }
+    cout << "#" << test_case << " " << max_ans<<"\n";
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
