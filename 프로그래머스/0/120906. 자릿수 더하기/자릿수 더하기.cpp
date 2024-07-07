ea7f2c3020dc6af51449142b3216a3fe33a56926
yoon-gang <sgi07151@naver.com>
2024-07-08 08:14:50 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    string s = to_string(n);
+    for(int i=0;i<s.length();i++){
+        answer += s[i]-'0';
+    }
+    return answer;
+}
