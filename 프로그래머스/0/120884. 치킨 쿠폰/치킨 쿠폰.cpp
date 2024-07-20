a9bbac90f7324c1cf87f356e628340929e0a5762
yoon-gang <sgi07151@naver.com>
2024-07-20 23:57:44 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int chicken) {
+    int answer = 0;
+    while(chicken/10!=0){
+        answer += chicken/10;
+        chicken = (chicken/10 + chicken%10);
+    }
+    
+    return answer;
+}
