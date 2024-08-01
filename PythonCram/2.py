c33108e52e15490b11f62bbd50150d037244bcf2
yoon-gang <sgi07151@naver.com>
2024-08-01 13:06:47 +0900

    1~20

---
+++ PythonCram/2.py
@@ -0,0 +1,40 @@
+# 삼성전자 = 50000
+# 총평가금액 = 삼성전자 * 10
+# print(총평가금액)
+#----------------------------
+# 시가총액 = 289000000000000
+# 현재가 = 50000
+# PER = 15.79
+# print(시가총액,type(시가총액))
+# print(현재가,type(현재가))
+# print(PER,type(PER))
+#----------------------------
+# s="hello"
+# t="python"
+# print(s,"! ",t,sep="")
+# print(s+"!",t)
+#----------------------------
+# print(2+2*3)
+#----------------------------
+# a=128
+# print(type(a))
+# a="132"
+# print(type(a))
+#----------------------------
+# num_str="720"
+# num_int=int(num_str)
+# print(num_int+1,type(num_int))
+#----------------------------
+# num=100
+# num_str = str(num)
+# print(num_str,type(num_str))
+#----------------------------
+# str = "15.79"
+# flo = float(str)
+# print(flo,type(flo))
+#----------------------------
+# year = "2020"
+# print(int(year)-3)
+# print(int(year)-2)
+# print(int(year)-1)
+#----------------------------
