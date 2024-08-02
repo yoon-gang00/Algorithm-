83253b26c44a289bee0d1afdf850bc4d56bb8b1d
yoon-gang00 <yk1008@konkuk.ac.kr>
2024-08-02 17:42:03 +0900

    python 21~30

---
+++ PythonCram/3.py
@@ -0,0 +1,38 @@
+#021~030 파이썬 문자열
+# letters = 'python'
+# print(letters[0],letters[2])
+#-------------------------------
+# license_plate = "24가 2210"
+# print(license_plate[4:])
+#-------------------------------
+# string = "홀짝홀짝홀짝"
+# print(string[::2]) 세번째 매개변수는 step(슬라이싱 시 건너뛰는 간격)
+#-------------------------------
+# string = "PYTHON"
+# print(string[::-1])
+#-------------------------------
+# phone_number = "010-1111-2222"
+# x = phone_number.replace("-"," ")
+# print(x)
+#-------------------------------
+# y = phone_number.replace("-","")
+# print(y)
+#-------------------------------
+# url = "http://sharebook.kr"
+# print(url[-2:])
+# url_split = url.split('.')
+# print(url_split[-1])
+#-------------------------------
+# lang='python'
+# lang[0]='P'
+# print(lang)
+# 문자열 수정 불가
+#-------------------------------
+# string = 'abcdfe2a354a32a'
+# string = string.replace('a','A')
+# print(string)
+#-------------------------------
+# string = 'abcd'
+# string = string.replace('b','B')
+# print(string)
+#-------------------------------
