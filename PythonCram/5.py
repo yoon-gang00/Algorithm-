bf2d417db361706333ddb6046af6ca2ca38ea34c
yoon-gang00 <yk1008@konkuk.ac.kr>
2024-08-02 18:24:05 +0900

    041~051

---
+++ PythonCram/5.py
@@ -0,0 +1,36 @@
+# 041~051
+# ticker = "btc_krw"
+# ticker = ticker.upper()
+# print(ticker)
+#-------------------------
+# ticker = "BTC_KRW"
+# ticker = ticker.lower()
+# print(ticker)
+#-------------------------
+# x="Hello"
+# x=x.capitalize()
+# print(x)
+#-------------------------
+# file_name="보고서.xlsx"
+# file_name.endswith(("xlsx","xls"))
+#-------------------------
+# file_name = "2020_보고서.xlsx"
+# x= file_name.startswith("2020")
+# print(x)
+#-------------------------
+# a = "hello world"
+# b = a.split(" ")
+# print(b)
+#-------------------------
+# ticker = "btc_krw"
+# b = ticker.split("_")
+# print(b)
+#-------------------------
+# date = "2020-05-01"
+# a = date.split("-")
+# print(a)
+#-------------------------
+data = "039490     "
+data = data.rstrip()
+print(data)
+#-------------------------
