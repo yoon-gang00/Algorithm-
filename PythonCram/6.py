b211713d5f600509ebdb930236425ec4dbf4817f
yoon-gang00 <yk1008@konkuk.ac.kr>
2024-08-02 18:44:03 +0900

    051~060

---
+++ PythonCram/6.py
@@ -0,0 +1,37 @@
+#051~061 파이썬 리스트
+#-------------------------
+# movie_rank = ["닥터 스트레인지","스플릿","럭키"]
+# #-------------------------
+# movie_rank.append("베트맨")
+# print(movie_rank)
+# #-------------------------
+# movie_rank.insert(1,"슈퍼맨")
+# print(movie_rank)
+# #-------------------------
+# movie_rank.remove("럭키")
+# print(movie_rank)
+# #-------------------------
+# del movie_rank[2]
+# del movie_rank[2]
+# print(movie_rank)
+#-------------------------
+lang1 = ["C", "C++", "JAVA"]
+lang2 = ["Python", "Go", "C#"]
+langs = lang1+lang2
+print(langs)
+#-------------------------
+nums = [1,2,3,4,5,6,7]
+print("max: ",max(nums))
+print("min: ",min(nums))
+#-------------------------
+nums = [1,2,3,4,5]
+print(sum(nums))
+#-------------------------
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+print(len(cook))
+#-------------------------
+num=[1,2,3,4,5]
+avg = sum(nums)/len(nums)
+print(avg)
+#-------------------------
+#-------------------------
