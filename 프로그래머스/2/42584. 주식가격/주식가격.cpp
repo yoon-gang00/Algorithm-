25000d0ee80de40f20a480fe7f6372a4110e1cbc
yoon-gang <sgi07151@naver.com>
2024-10-14 20:44:15 +0900

    [level 2] Title: 주식가격, Time: 27.18 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,5 +1,32 @@
 #include <string>
 #include <vector>
+#include <stack>
+using namespace std;
+//stack사용해서 풀어보기
+vector<int> solution(vector<int> prices) {
+    int n = prices.size();
+    vector<int> answer(n,0);
+    stack<int> s;
+    for(int i = 0;i<n;i++){
+        while(!s.empty() && prices[s.top()]>prices[i]){
+            int top = s.top();
+            s.pop();
+            answer[top] = i - top;
+        }
+        s.push(i);
+    }
+    while(!s.empty()){
+        int top = s.top();
+        s.pop();
+        answer[top] = n-1-top;
+    }
+    return answer;
+}
+
+
+/* 시간복잡도 O(n^2)
+#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -16,3 +43,4 @@ vector<int> solution(vector<int> prices) {
     }
     return answer;
 }
+*/
